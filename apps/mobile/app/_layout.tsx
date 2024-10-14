62b2be2cbc7c9acafb7588ee351bdc4a1962f2cb
Yusef Qazi <yqazi27@gmail.com>
2024-10-14 08:41:31 -0400

    Added details route

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
     <TRPCReactProvider>
       <Stack>
         <Stack.Screen name="index" />
+        <Stack.Screen name="details" />
       </Stack>
     </TRPCReactProvider>
   );
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,18 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 import { trpc } from "../utils/trpc";
 
 export default function Index() {
   const { data } = trpc.get.useQuery({ name: "John Doe" });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen. {data?.message}</Text>
+      <Link href="/details">View details</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
